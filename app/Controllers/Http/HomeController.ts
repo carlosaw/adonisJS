7a8ec44ab61c5958cbf217082ed4c767fc23f004
unknown <carlosfreevg@gmail.com>
2022-05-01 10:53:23 -0400

    Instalando o mysql

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -1,14 +1,14 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+//import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class HomeController {
 
   async index({view}) {
-    return view.render('welcome');
-    //return 'Hello-World';
+       
+    return view.render('homepage');
   }
 
-  async sobre() {
-    return 'Sobre Nós';
+  async sobre({view}) {
+    return view.render('sobre');
   }
 
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,4 @@
 import Route from '@ioc:Adonis/Core/Route'
-import './api.routes';
 
 Route.get('/', 'HomeController.index')
 Route.get('/sobre', 'HomeController.sobre')
